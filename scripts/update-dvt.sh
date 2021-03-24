55ee824bf9397b3d148d95c6737f51b654cf0a05
Nicola Zambello <nzambello@protonmail.com>
2021-03-24 10:23:02 +0100

    Update update-dvt.sh

--- scripts/update-dvt.sh
+++ scripts/update-dvt.sh
@@ -3,4 +3,4 @@
 echo -n "Enter design-volto-theme version: "
 read -r dvtVersion
 
-npx purge-lockfile@latest volto && rm -rdf node_modules && yarn cache clean && yarn add "redturtle/design-volto-theme#v$dvtVersion" -W && yarn i18n && git commit -am "chore(upd8): update dvt" && rm -rdf node_modules && yarn install --frozen-lockfile
+npx purge-lockfile@latest volto && rm -rdf node_modules && yarn cache clean && yarn add "redturtle/design-volto-theme#v$dvtVersion" -W && yarn i18n && git commit -am "chore(upd8): update dvt v$dvtVersion" && rm -rdf node_modules && yarn install --frozen-lockfile
